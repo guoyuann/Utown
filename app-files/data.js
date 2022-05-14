e56474c7bd063270b9d202f4f68e79a1596a1a25
guoyuann <may.liguoyuan@gmail.com>
2022-05-14 13:14:08 +0800

    Initial commit

---
+++ app-files/data.js
@@ -0,0 +1,182 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-erc_level-1_nearescalator_photo_1512",
+      "name": "ERC_Level 1_NearEscalator_PHOTO_1512",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.123794584022253,
+          "pitch": -0.009543834019449804,
+          "rotation": 16.493361431346422,
+          "target": "1-erc_level-1_starbucks"
+        },
+        {
+          "yaw": -2.6247077052330745,
+          "pitch": 0.09590941920661145,
+          "rotation": 0.7853981633974483,
+          "target": "2-erc_level-1_tembusu-tree"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.07826570213022066,
+          "pitch": 0.3082052769551993,
+          "title": "ERC Level 1&nbsp;",
+          "text": "Welcome to the open space on level 1 of Education Resource Centre!&nbsp;"
+        },
+        {
+          "yaw": -1.0065174703649724,
+          "pitch": -0.006646332350326745,
+          "title": "Staircase",
+          "text": "Take the staircase down to Starbucks for drinks/food!"
+        }
+      ]
+    },
+    {
+      "id": "1-erc_level-1_starbucks",
+      "name": "ERC_Level 1_Starbucks",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5770755608835376,
+          "pitch": 0.007071187147879243,
+          "rotation": 5.497787143782138,
+          "target": "0-erc_level-1_nearescalator_photo_1512"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.5146151644018184,
+          "pitch": 0.06723457345475481,
+          "title": "ERC Starbucks",
+          "text": "Welcome to Starbucks in Utown, you can order something and study all day alone or with friends!"
+        },
+        {
+          "yaw": -0.056025185180200054,
+          "pitch": 0.3322136799865696,
+          "title": "Study Area",
+          "text": "A nature-lover? Study in this open space with natural sunlight which is just next to Utown Green!"
+        },
+        {
+          "yaw": -0.5721504871404299,
+          "pitch": 0.13292173910874894,
+          "title": "Staircase",
+          "text": "Take the staircase up to Level 1 Open space!"
+        }
+      ]
+    },
+    {
+      "id": "2-erc_level-1_tembusu-tree",
+      "name": "ERC_Level 1_Tembusu Tree",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0703554414684833,
+          "pitch": 0.2061491837248859,
+          "rotation": 0.7853981633974483,
+          "target": "0-erc_level-1_nearescalator_photo_1512"
+        },
+        {
+          "yaw": 2.6022762496189182,
+          "pitch": 0.10616758012668015,
+          "rotation": 4.71238898038469,
+          "target": "1-erc_level-1_starbucks"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.6016490176954674,
+          "pitch": -0.006151007004072895,
+          "title": "Staircase to Starbucks",
+          "text": "Take the stairs down to Starbucks and open study area!"
+        },
+        {
+          "yaw": -2.9761465330230443,
+          "pitch": -0.16282124462246017,
+          "title": "Tembusu tree",
+          "text": "AKA Crytophyllum fragrans"
+        }
+      ]
+    }
+  ],
+  "name": "Utown",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
